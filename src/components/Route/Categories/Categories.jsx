56a6bba40b107f5d2eb05b71ffaf1d0041f4e6b7
Sourav6803 <106535807+Sourav6803@users.noreply.github.com>
2023-10-29 07:20:10 +0530

    Update Categories.jsx

--- src/components/Route/Categories/Categories.jsx
+++ src/components/Route/Categories/Categories.jsx
@@ -9,7 +9,7 @@ const Categories = () => {
     const navigate = useNavigate()
     return (
         <>
-            <div className={`${styles.section} hidden sm:block`}>
+            <div className={`${styles.section} hidden `}>
                 <div className={`branding my-12 flex justify-between w-full shadow-sm bg-white p-5 rounded-md`}>
                     {
                         brandingData && brandingData.map((i, index) => (
