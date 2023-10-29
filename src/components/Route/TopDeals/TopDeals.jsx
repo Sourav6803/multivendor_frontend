41cf89a7f484956b67a91ef2992e1712a38e764c
Sourav6803 <106535807+Sourav6803@users.noreply.github.com>
2023-10-29 22:23:17 +0530

    Update TopDeals.jsx

--- src/components/Route/TopDeals/TopDeals.jsx
+++ src/components/Route/TopDeals/TopDeals.jsx
@@ -27,8 +27,8 @@ const TopDeals = () => {
 
 
   return (
-    <div>
-      <div className={`${styles.section} border-red-800`} style={{backgroundImage: "url('https://media.istockphoto.com/id/1180447175/vector/rangoli-dark-red-maroon-colored-grunge-background-diwali-greeting-with-one-small-diya-at-the.jpg?s=2048x2048&w=is&k=20&c=f7wcjVwr8LK45i26-wI7TuHTVeUEylfDN36IXhqq5fM=')" , backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
+    <div style={{backgroundImage: "url('https://media.istockphoto.com/id/1180447175/vector/rangoli-dark-red-maroon-colored-grunge-background-diwali-greeting-with-one-small-diya-at-the.jpg?s=2048x2048&w=is&k=20&c=f7wcjVwr8LK45i26-wI7TuHTVeUEylfDN36IXhqq5fM=')" , backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
+      <div className={`${styles.section} border-red-800`} >
         <div className={`${styles.heading}`}>
           <h1>Top Deals</h1>
         </div>
